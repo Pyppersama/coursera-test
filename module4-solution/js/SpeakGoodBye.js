89b3066773efdf55ce9a4a610494e1538ef37a1c
Godfrey-Offeh <offehuzezi09@gmail.com>
2023-02-12 07:51:19 +0100

    module 4 final assignment

---
+++ module4-solution/js/SpeakGoodBye.js
@@ -0,0 +1,14 @@
+(function(window){
+var helloSpeaker ={};
+
+  var speakWord = "Good Bye";
+
+  helloSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.helloSpeaker = helloSpeaker;
+})(window);
+
+
+
---
+++ module4-solution/js/SpeakHello.js
@@ -0,0 +1,11 @@
+(function(window){
+  var byeSpeaker = {};
+  var speakWord = "Hello";
+
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  }
+
+  window.byeSpeaker = byeSpeaker;
+})(window);
+
